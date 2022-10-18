7ec24fe9939281de89f7f3ff9cded32542755153
735880 <brettandrews3@gmail.com>
2022-10-18 10:31:34 -0500

    Revert "Add HttpPost to SignUp() on HomeController"
    This reverts commit 7d3fc2a67c3dc8ba1928d12d0baca1a9920a017c.

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using MVCApp.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -32,21 +31,6 @@ namespace MVCApp.Controllers
         public ActionResult SignUp()
         {
             ViewBag.Message = "Employee Sign Up";
-
-            return View();
-        }
-
-        // The View portion of the POST method. When I send data from the form, it'll send to this method
-        // [ValidateAntiForgeryToken] links to SignUp.cshtml and its .AntiForgeryToken on ln 12
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult SignUp(EmployeeModel model)
-        {
-            // Check that model complies w/ all annotation rules from EmployeeModel; extra security to go w/JS on client side
-            if (ModelState.IsValid)
-            {
-                return RedirectToAction("Index");
-            }
 
             return View();
         }
