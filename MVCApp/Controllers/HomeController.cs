21e11b45dfcd8a490a316798ae2d6070b3a4ff98
735880 <brettandrews3@gmail.com>
2022-10-20 10:32:05 -0500

    Data moves from front to back. Next: back to front.
    59:45 - 1:04:26
    
    HomeController has been built out to take in user input at the front end, send to backend, and insert in the DB. See code comments, Miro board, and notebook notes on p89 for full context.
    
    Next: move the data from the DB to the front end.

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static DataLibrary.BusinessLogic.EmployeeProcessor;
 
 namespace MVCApp.Controllers
 {
@@ -36,15 +37,24 @@ namespace MVCApp.Controllers
             return View();
         }
 
-        // The View portion of the POST method. When I send data from the form, it'll send to this method
+        // The View portion of the POST method. When I send data from the web form, it'll send to this method
         // [ValidateAntiForgeryToken] links to SignUp.cshtml and its .AntiForgeryToken on ln 12
+        // SignUp() is passing in the frontend MVCApp version of EmployeeModel, but it's using CreateEmployee() from EmployeeProcessor.
+        // This is also bridging the gap between front- and backend. We can use this CreateEmployee() like so because of the new using
+        // statement on ln 7 that's importing a static EmployeeProcessor class. 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult SignUp(EmployeeModel model)
         {
             // Check that model complies w/ all annotation rules from EmployeeModel; extra security to go w/JS on client side
+            // 1:00:00: ModelState is now valid
+            // Add 'using static DataLibrary.BusinessLogic.EmployeeProcessor' to just call CreateEmployee() directly
             if (ModelState.IsValid)
             {
+                int recordsCreated = CreateEmployee(model.EmployeeId, 
+                    model.FirstName, 
+                    model.LastName, 
+                    model.EmailAddress);
                 return RedirectToAction("Index");
             }
 
